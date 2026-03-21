3093c015f1747f566994b37f0a0d2a46611c19b5
Gregory Linscheid <linscheid.greg@gmail.com>
2026-03-21 15:46:34 -0700

    :shirt: Comment

--- src/milkTea.tsx
+++ src/milkTea.tsx
@@ -33,7 +33,7 @@ const RENDER_CONFIG: RenderConfig = {
   ...sizeFromVideoPreset(DEFAULT_VIDEO_SIZE_PRESET),
   fps: DEFAULT_VIDEO_FPS,
   bpp: DEFAULT_MAIN_RECORD_BPP,
-  formatId: VIDEO_FORMAT_OPTIONS[0].id,
+  formatId: VIDEO_FORMAT_OPTIONS[0].id, // MP4
   baseName: 'milktea'
 };
 
